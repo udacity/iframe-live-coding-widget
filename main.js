067adb40cc489abab10c59e930b537907ddeaf0e
James Williams <James.L.Williams@gmail.com>
2014-10-23 01:30:36 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,42 @@
+var toastGroupTemplate = document.querySelector('#toastGroup');
+toastGroupTemplate.showToast = function() {
+  document.querySelector('#toast').show();
+}
+
+var tabs = document.querySelector('paper-tabs');
+var pages = document.querySelector('core-pages');
+
+tabs.addEventListener('core-select',function(){
+  pages.selected = tabs.selected;
+  if (pages.selected === 0) {
+    pages.querySelectorAll("div")[0].style.display = "block";
+    pages.querySelectorAll("div")[1].style.display = "none";
+
+  } else if (pages.selected == 1) {
+    pages.querySelectorAll("div")[0].style.display = "none";
+    pages.querySelectorAll("div")[1].style.display = "block";
+  } else {
+    pages.querySelectorAll("div")[1].style.display = "none";
+  }
+});
+
+// Get Query String
+var queryDict = {}
+location.search.substr(1).split("&").forEach(function(item) {queryDict[item.split("=")[0]] = item.split("=")[1]})
+
+
+// Run code
+var runButton = document.querySelector('#btnRun');
+var editor = document.querySelector('#editor');
+runButton.addEventListener('click', function(e) {
+  var content = editor.mirror.getValue();
+  console.log(content);
+  eval(content);
+});
+
+// Load code
+window.addEventListener('polymer-ready', function(e) {
+  var exercise = queryDict["exercise"];
+  var content = stubs[exercise];
+  editor.mirror.setValue(content);
+});
